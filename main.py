d96a601d015046749eebeac7a76780ccd9b4cdfa
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-26 21:31:24 -0600

    Dataset class definition

--- main.py
+++ main.py
@@ -69,4 +69,38 @@ def show_random(images: List[Path], masks: List[Path], figname: str, transforms=
 
 show_random(ALL_IMAGES, ALL_MASKS, "figure1.png")
 
+from typing import List
+from torch.utils.data import Dataset
+
+class SegmentationDataset(Dataset):
+    def __init__(
+            self,
+            images: List[Path],
+            masks: List[Path] = None,
+            transforms = None
+    ) -> None:
+        self.images = images
+        self.masks = masks
+        self.transforms = transforms
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def __getitem__(self, idx: int) -> dict:
+        image_path = self.images[idx]
+        image = utils.imread(image_path)
+
+        result = {"image": image}
+
+        if self.masks is not None:
+            mask = gif_imread(self.masks[idx])
+            result["mask"] = mask
+
+        if self.transforms is not None:
+            result = self.transforms(**result)
+
+        result["filename"] = image_path.name
+
+        return result
+
 logging.info("Done!")
